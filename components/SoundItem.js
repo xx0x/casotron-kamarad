70d7762bd0e8829389d3c5375848431785c5b4a9
Vaclav Mach <vaclav@xproduction.cz>
2021-05-17 13:58:33 +0200

    basic sound conversion

---
+++ components/SoundItem.js
@@ -0,0 +1,49 @@
+import audioBufferToWav from "audiobuffer-to-wav";
+import { useState } from "react";
+import { WaveFile } from "wavefile";
+
+
+export default function SoundItem({ id, filename }) {
+
+    const [audioSrc, setAudioSrc] = useState(null);
+
+    return (
+        <div key={id}>
+            <h3>{id}</h3>
+            <audio
+                src={audioSrc}
+                controls
+            />
+
+            <button onClick={() => {
+                const audioContext = new window.AudioContext();
+                const request = new XMLHttpRequest();
+                request.open('GET', filename, true);
+                request.responseType = 'arraybuffer';
+                request.onload = function () {
+                    audioContext.decodeAudioData(request.response).then((decodedAudio) => {
+                        console.log(decodedAudio);
+                        const wavData = audioBufferToWav(decodedAudio);
+                        console.log(wavData);
+                        // setAudioSrc(URL.createObjectURL(new Blob([wavData])));
+                        const wavFile = new WaveFile(new Uint8Array(wavData));
+                        wavFile.toBitDepth(16);
+                        wavFile.toSampleRate(22050);
+                        console.log(wavFile);
+
+                        const convertedWavData = wavFile.toBuffer();
+                        //                         console.log(convertedWavData);
+                        setAudioSrc(URL.createObjectURL(new Blob([convertedWavData])));
+                    }).catch((e) => {
+                        console.log("Error with decoding audio data", e);
+                    });
+
+                }
+                request.send();
+
+            }}>
+                Load default
+            </button>
+        </div >
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
 import Header from '../components/Header'
 import SerialDeviceInfo from '../components/SerialDeviceInfo';
+import SoundItem from '../components/SoundItem';
+import { getAllSounds } from '../lib/sounds';
 
-export default function Home() {
 
+
+export async function getStaticProps() {
+    return {
+        props: {
+            sounds: getAllSounds()
+        }
+    }
+}
+
+export default function Home(props) {
     return (
         <>
             <Header />
             <SerialDeviceInfo />
+
+            {props.sounds.map((sound) => (
+                <SoundItem {...sound} key={sound.id} />
+            ))}
         </>
     );
-
 }
