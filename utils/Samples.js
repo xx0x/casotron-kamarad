b1899d6d8254093cc5cd291035c407950ae996ba
Vaclav Mach <vaclav@xproduction.cz>
2021-05-31 16:16:55 +0200

    add random time testing

---
+++ utils/Samples.js
@@ -0,0 +1,19 @@
+const Samples = {
+    Intro: 60,
+    Outro: 61,
+    Hours: 70,
+    Hours1: 71,
+    Hours24: 72,
+    Minutes: 80,
+    Minutes1: 81,
+    Minutes24: 82,
+    Alarm1: 100,
+    AlarmBase: 100,
+    Warning: 90,
+    TimeSet: 91,
+    TimeCurrent: 92,
+    AlarmSet: 93,
+    AlarmCurrent: 94
+};
+
+export default Samples;
